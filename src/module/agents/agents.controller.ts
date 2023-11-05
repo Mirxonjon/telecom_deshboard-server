1a444c5bb135f5c1c2f91c4a4d2b72f58da58fd7
Mirxonjon <mirxonjonismanov152@gmail.com>
2023-11-05 10:07:46 +0500

    add ser

--- src/module/agents/agents.controller.ts
+++ src/module/agents/agents.controller.ts
@@ -1,5 +1,5 @@
-import { Controller , Get, Query } from "@nestjs/common";
-import { ApiBadRequestResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { Body, Controller , Get, HttpCode, HttpStatus, Post, Query ,Patch ,Param ,} from "@nestjs/common";
+import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { AgentsService } from "./agents.service";
 
 @Controller('agents')
@@ -9,7 +9,13 @@ export class AgentsController {
   constructor(service: AgentsService) {
     this.#_service = service;
   }
-
+  @Get('agents/all')
+  @ApiBadRequestResponse()
+  @ApiNotFoundResponse()
+  @ApiOkResponse()
+  async findAllAgents() {
+    return await this.#_service.findAllAgents();
+  }
   @Get('all')
   @ApiBadRequestResponse()
   @ApiNotFoundResponse()
@@ -25,5 +31,79 @@ export class AgentsController {
   @Query('operator_number') operator_number: string,
   @Query('status') status: string,) {
     return await this.#_service.filterAll(name , operator_number , status);
+  }
+
+  @Post('create/service')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiBody({
+    schema: {
+      type: 'object',
+      required: [
+        'service_id',
+      ],
+      properties: {
+        service_id: {
+          type: 'string',
+          default: 'acds',
+        },
+        
+      },
+    },
+  })
+  async createService(@Body() body: {service_id : string})  {
+    return this.#_service.createService(body)        
+  }
+
+  @Post('create/group')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiBody({
+    schema: {
+      type: 'object',
+      required: [
+        'service_id',
+        'group_id',
+        'name',
+        'title'
+      ],
+      properties: {
+        service_id: {
+          type: 'string',
+          default: 'acds',
+        },
+        group_id: {
+          type: 'string',
+          default: 'acds',
+        },
+        name: {
+          type: 'string',
+          default: 'acds',
+        }, 
+        title: {
+          type: 'string',
+          default: 'acds',
+        },
+        
+      },
+    },
+  })
+  async createGroup(@Body() body: {service_id : string,group_id : string,name : string,title : string})  {
+    return this.#_service.createGroup(body)        
+  }
+
+  @Patch('/updateAgent/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'string',
+          default: 'True',
+        },
+      },
+    },
+  })
+  async update(@Param('id') id: string, @Body() updateAgentdto: {status: boolean}) {
+    return this.#_service.updateAgent(id, updateAgentdto);
   }
 }
--- src/module/agents/agents.service.ts
+++ src/module/agents/agents.service.ts
@@ -1,9 +1,21 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { agentsDataStateEntity } from 'src/entities/agentsDataState.entity';
 import { agentslockEntity } from 'src/entities/agentslock.entity';
+import { GroupsEntity } from 'src/entities/group.entity';
+import { ServicesEntity } from 'src/entities/service.entity';
 import { Like } from 'typeorm';
 
 @Injectable()
 export class AgentsService {
+  async findAllAgents ( ) {
+    const findBlockAgents = await agentsDataStateEntity.find({
+        order : {
+            create_data : 'DESC'
+        }
+    })
+
+    return findBlockAgents
+}
     async findAll ( ) {
         const findBlockAgents = await agentslockEntity.find({
             order : {
@@ -31,5 +43,66 @@ export class AgentsService {
 
         return filteragents
     }
+   async  createService(body: {service_id : string} ) {
+    await ServicesEntity.createQueryBuilder()
+    .insert()
+    .into(ServicesEntity)
+    .values({
+      service_id: body.service_id
+    })
+    .execute()
+    .catch((e) => {
+      console.log(e);
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+    });
+
+    }
+
+    async  createGroup(body: {service_id : string,group_id : string,name : string,title : string} ) {
+      const  findService = await ServicesEntity.findOneBy({id:body.service_id})
+
+      if(!findService) {
+        throw new HttpException('Not Found Service', HttpStatus.BAD_REQUEST);
+        
+      }
+      await GroupsEntity.createQueryBuilder()
+      .insert()
+      .into(GroupsEntity)
+      .values({
+        name :body.name,
+        title: body.title,
+        group_id: body.group_id,
+        servic : body.service_id as any
+      })
+      .execute()
+      .catch((e) => {
+        console.log(e);
+        throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+      });
+  
+      }
+
+      async updateAgent(id : string , body: {status: boolean}) {
+        const findAgent = await agentsDataStateEntity.findOne({
+          where: {
+            id: id,
+          },
+        }).catch(() => {
+          throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+        });
+    
+        if (findAgent) {
+          await agentsDataStateEntity.createQueryBuilder()
+            .update(agentsDataStateEntity)
+            .set({
+              IsSupervazer: body.status
+            })
+            .where({ id })
+            .execute()
+            .catch(() => {
+              throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+            });
+        }
+      }
 
 }
